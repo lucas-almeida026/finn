348c608a0d90ee47f107afa5c48ba708b34c38c6
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2024-07-02 13:23:19 -0300

    redirect to create account page if user has no accounts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises'
 import express from 'express'
 import { startAPI } from './api'
 import axios from 'axios'
-import ejs from 'ejs';
+import ejs from 'ejs'
 
 const app = express();
 
@@ -12,22 +12,27 @@ const app = express();
 	const api = axios.create({
 		baseURL: `http://localhost:${port}`
 	})
-	
+
 	const tailwindcss = await fs.readFile('./src/styles/output.css', 'utf8')
 	const baseTemplate = ejs.compile(await fs.readFile('./src/templates/base.ejs', 'utf8'))
 	const indexPage = ejs.compile(await fs.readFile('./src/templates/index.ejs', 'utf8'))
+	const createAccPage = ejs.compile(await fs.readFile('./src/templates/create-account.ejs', 'utf8'))
 
 	function page(title: string, body: ejs.TemplateFunction, data: any) {
-		return baseTemplate({title, body: body(data), tailwindcss})
+		return baseTemplate({ title, body: body(data), tailwindcss })
 	}
 
 	app.get('/', async (req, res) => {
-		const {data: accounts} = await api.get('/accounts')
+		const { data: accounts } = await api.get('/accounts')
 		return res.send(page('Home', indexPage, { accounts }))
 	})
 
 	app.get('/account-list', async (req, res) => {
 		return setTimeout(() => res.send('<p>accounts list</p>'), 1000)
+	})
+
+	app.get('/account/create', (req, res) => {
+		return res.send(page('Create Account', createAccPage, {}))
 	})
 
 	app.listen(2900, () => console.log('Client started on port 2900'))
