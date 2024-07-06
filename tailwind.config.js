005ba34a208d7b8212f3baa965b48ad24fa04775
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2024-07-06 19:58:44 -0300

    fix tailwind build of conditionally used classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ejs,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{ejs,ts}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	safelist: [
+		{
+			pattern: /(bottom|top|left|right|rotate)-[0-9]+/
+		},
+		{
+			pattern: /-?z-[0-9]+/
+		}
+	]
 }
 
